449e5626c808747e2e8838d0c7f7e03146c8699b
Jasper Abbink <git@abb.ink>
2020-02-26 20:12:55 +0100

    Change to pub.ewi.tudelft.nl
    original pub.etv.tudelft.nl doesn't send a complete certificate chain anymore

--- scripts/pubtimealert.js
+++ scripts/pubtimealert.js
@@ -30,7 +30,7 @@ module.exports = function(robot) {
     const todaysStartTime = now.getTime();
 
     try {
-      const calendar = await fetch('https://pub.etv.tudelft.nl/ical/feed/all');
+      const calendar = await fetch('https://pub.ewi.tudelft.nl/ical/feed/all');
       const calendarEvents = ical.parseICS((await calendar.text()).split('\n').map(_ => {return _.trim()}).join('\n'));
 
       const event = Object.values(calendarEvents)
